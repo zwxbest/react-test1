99daf02a4a912511bdce47ca9edac39846c0190b
zwxbest <zhangweixiao@live.com>
2019-02-28 20:39:53 +0800

    add

--- src/TodoList.js
+++ src/TodoList.js
@@ -41,7 +41,11 @@ class TodoList extends Component {
     }
 
     handleItemDelete(index){
-        console.log(index);
+        const list = [...this.state.list];
+        list.splice(index,1);
+        this.setState({
+            list: list
+        })
     }
 }
 
