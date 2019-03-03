7b287bb55dd5153f177ef6d723dbfe59e70b30a6
zwxbest <zhangweixiao@live.com>
2019-03-03 17:00:04 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+import App from './App';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
