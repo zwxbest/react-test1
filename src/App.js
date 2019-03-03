75393d6b62358ab0e45e32501ad417c949f35645
zwxbest <zhangweixiao@live.com>
2019-03-03 17:48:34 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import  React,{Component,Fragment} from 'react';
-import {CSSTransition} from 'react-transition-group';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 import './style.css'
 
 class App extends  Component{
@@ -7,29 +7,41 @@ class App extends  Component{
     constructor(props){
         super(props);
         this.state={
-            show: true
+            list: []
         }
-        this.handleToggle=this.handleToggle.bind(this);
+        this.handleAddItem=this.handleAddItem.bind(this);
     }
 
     render(){
         return(
             <Fragment>
-                {/*classNames对应style.css中的fade-enter的前缀*/}
-                <CSSTransition in={this.state.show} timeout={1000} classNames="fade"
-                onEntered={(el)=>{el.style.color='blue'}} appear={true}>
-                    {/*appear={true}一开始就有动画。加一个fade-apper和fade-appear-active*/}
-                    <div>hello</div>
-                </CSSTransition>
-            <button onClick={this.handleToggle}></button>
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                /*classNames对应style.css中的fade-enter的前缀*/
+                                 <CSSTransition timeout={1000} classNames="fade" key={index}
+                                           onEntered={(el)=>{el.style.color='blue'}} appear={true}>
+                                {/*appear={true}一开始就有动画。加一个fade-apper和fade-appear-active*/}
+                                <div >{item}</div>
+                            </CSSTransition>
+
+                            )
+                        })
+                    }
+                </TransitionGroup>
+
+            <button onClick={this.handleAddItem}></button>
             </Fragment>
         )
     }
 
-    handleToggle(){
-        this.setState(
+    handleAddItem (){
+        this.setState((preState )=>
             {
-                show: !this.state.show
+                return{
+                    list: [...preState.list,'item']
+                }
             }
         )
     }
