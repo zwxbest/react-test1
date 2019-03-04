9cc3f3c5358ccd3fbfa93c4c6fed381d7331ba88
zwxbest <zhangweixiao@live.com>
2019-03-04 20:00:17 +0800

    add

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,25 @@ import 'antd/dist/antd.css'
 import {Input, Button, List} from 'antd'
 import store from './store/'
 import {getInputChangeAction,getAddToDoItemAction,getDeleteToDoItemAction}  from "./store/actionCreators";
-import TodoListUI from './TodoListUI'
+
+const TodoListUI =  (props)=>{
+        return (
+            <div style={{marginLeft: '10px', marginTop: '10px'}}>
+                <div>
+                    <Input value={props.inputValue} placeholder="todo info"
+                           style={{width: '300px', marginRight: '10px'}}
+                           onChange={props.handleInputChange}/>
+                    <Button type="primary" onClick={props.handleButtonClick}>提交</Button>
+                </div>
+                <List style={{marginTop: '10px', width: '300px'}}
+                      bordered
+                      dataSource={props.list}
+                      renderItem={(item, index) => (<List.Item
+                          onClick={(index)=>{props.handleItemClick(index)}}>{item}</List.Item>)}
+                />
+            </div>
+        )
+};
 
 class TodoList extends Component {
     constructor(props) {
