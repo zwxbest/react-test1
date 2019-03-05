1dffca5add635e0a536f848fe7ee0cb4f7d4ff8f
zhangweixiao <zhangweixiao@live.com>
2019-03-05 21:00:11 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 1. saga也能接收到`const action =getInitList();`,sagas.js接收type为`GET_INIT_LIST`
-2. 结果会打印出abc
+2. yield axios和yield put
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,12 +1,17 @@
-import {takeEvery} from 'redux-saga/effects'
+import {takeEvery,put} from 'redux-saga/effects'
 import {GET_INIT_LIST} from "./actionTypes";
+import axios from 'axios';
+import {initListAction} from './actionCreators'
 
 function * todoSagas() {
    yield takeEvery(GET_INIT_LIST,getInitList);
 }
 
 function * getInitList() {
-    console.log('abc')
+    const res = yield axios.get('http://47.92.161.49:3000/mock/11/api');
+    const data = res.data;
+    const action=initListAction(data);
+    yield put(action);
 }
 
 
